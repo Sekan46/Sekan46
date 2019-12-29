68d26d979bd16155f8852f43dd6d4874e8bccff6
Sekan46 <2055241018@qq.com>
2019-12-29 10:23:43 +0800

    work19

--- work19/index.js
+++ work19/index.js
@@ -1,22 +1,23 @@
-let ws
-function connect(){
-    let server = document.getElementById('server').value
+let ws 
+function connect() {
+    let server = document.getElementById('server').value 
     ws = new WebSocket(server);
     ws.onopen = function(){
-        document.getElementById('conn').disabled='disable';
+        document.getElementById('conn').disabled="disable";
         document.getElementById('disconn').disabled='';
         let nickname=document.getElementById('nickname').value
         if(nickname){
             ws.send('nickname|'+nickname)
         }
+
     }
     ws.onclose = function(){
-        document.getElementById('conn').disable='';
-        document.getElementById('disconn').disable= 'disable';
+        document.getElementById('conn').disabled='';
+        document.getElementById('disconn').disabled= 'disable';
     }
-    ws.onmessage =function(event){
+    ws.onmessage=function(event){
         let board = document.getElementById('board')
-        let newmsg = document.createComment('div')
+        let newmsg = document.createElement('div')
         console.log(event.data)
         newmsg.innerHTML= event.data
         board.appendChild(newmsg)
@@ -24,11 +25,11 @@ function connect(){
     }
 }
 
-function disconnect(){
+function disconnent(){
     ws.close()
 }
 
 function send(){
-let msg = document.getElementById('content').value
-ws.send(msg)
+    let msg = document.getElementById('content').value
+    ws.send(msg)
 }
